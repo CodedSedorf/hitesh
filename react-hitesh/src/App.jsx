940c350753912ad7469afc2190b489497da77c91
CodedSedorf <sedorfdlc@gmail.com>
2025-05-01 03:09:15 +0100

    password generator

--- react-hitesh/src/App.jsx
+++ react-hitesh/src/App.jsx
@@ -6,6 +6,7 @@ import image1 from "../src/assets/teacher16.png";
 import image2 from "../src/assets/teacher17.png";
 import image3 from "../src/assets/teacher18.png";
 import image4 from "../src/assets/teacher19.png";
+import PinGenerator from "./Component/PinGenerator";
 const App = () => {
   const [counter, setcounter] = useState(1);
   return (
@@ -26,6 +27,7 @@ const App = () => {
       <Card role="Backend" year="2024" image={image2} />
       <Card role="Figma-Designer" year="2014" image={image3} />
       <Card role="Fullstack" year="2025" image={image4} />
+      <PinGenerator />
     </>
   );
 };
